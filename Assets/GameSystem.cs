0ed0ba69215142f2001f173b134cb61a09efd22d
Arne Zerndt <arne.zerndt@student.hpi.de>
2020-02-29 15:36:11 +0100

    Add WebsocketSharp and Gamesystem Script.

---
+++ Assets/GameSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+public class GameSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        using (var ws = new WebSocket ("ws://localhost:25566/ws")) {
+            ws.OnMessage += (sender, e) =>
+                Debug.Log("Received: " + e.Data);
+
+            ws.Connect ();
+            ws.Send ("WS Connected");
+            Debug.Log("WS Connected");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
