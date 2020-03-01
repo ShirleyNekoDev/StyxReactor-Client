b171403ea475ce47a7a8a2025103e17772e04f7a
Maximilian Stiede <maximilian.stiede@student.hpi.uni-potsdam.de>
2020-03-01 14:32:02 +0100

    add server base pings

--- Assets/GameSystem.cs
+++ Assets/GameSystem.cs
@@ -10,12 +10,18 @@ public class GameSystem : MonoBehaviour {
     private class Message {
         public String command;
         public dynamic payload;
+        public Message (String _command, dynamic _payload) {
+            command = _command;
+            payload = _payload;
+        }
     }
 
     public GameObject FloorPlate;
     public GameObject Wall;
     public GameObject Stealth;
     public GameObject Ping;
+
+    public String player = "Local Player";
 
     private WebSocket ws;
 
@@ -26,6 +32,7 @@ public class GameSystem : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start () {
+        // todo could not connect warning
         ws = new WebSocket ("ws://localhost:25566/ws");
         ws.OnMessage += (sender, e) => {
             Debug.Log ("Received: " + e.Data);
@@ -41,23 +48,47 @@ public class GameSystem : MonoBehaviour {
 
     }
 
+    class Position {
+        public int x;
+        public int y;
+        public Position (int _x, int _y) {
+            x = _x;
+            y = _y;
+        } 
+    }
+
+    class PingPayload {
+        public Position position;
+        public String type;
+        public String player;
+        public PingPayload (Position _position, String _type, String _player) {
+            position = _position;
+            type = _type;
+            player = _player;
+        }
+    }
+
     // Sending
     void SendPing (Vector3 position) {
         Debug.Log ("send Ping Log");
         int x = (int) position.x;
         int z = (int) position.z;
-        String message = "Test";
-        String blueprint = $@"{{
-             ""command"":""ping"",
-             ""payload"":
-             ""position"": {x},{z},
-             ""type"":{message}
-             }}";
-        //string sending = string.Format(blueprint, x.ToString(), y.ToString(), message);
-        Debug.Log("Gotthisfar");
-        Debug.Log(blueprint); 
-        ws.Send (blueprint);
-        InstantiatePing (new Vector3(x, 0, z));
+        // String message = "Test";
+        // String blueprint = $@"{{
+        //      ""command"":""ping"",
+        //      ""payload"":
+        //      ""position"": {x},{z},
+        //      ""type"":{message}
+        //      }}";
+        var message = new Message(
+            "ping",
+            new PingPayload(
+                new Position(x, z),
+                "ATTENTION",
+                player
+            )
+        );
+        ws.Send (JsonConvert.SerializeObject(message));
     }
 
     // Receiving
@@ -65,6 +96,9 @@ public class GameSystem : MonoBehaviour {
         switch (message.command) {
             case "set_world":
                 this.InstantiateWorld (message.payload);
+                break;
+            case "ping":
+                this.InstantiatePing (message.payload);
                 break;
             default:
                 break;
@@ -110,11 +144,17 @@ public class GameSystem : MonoBehaviour {
         }
     }
 
-    void InstantiatePing (Vector3 position) {
-        Instantiate(
-            Ping,
-            position,
-            Quaternion.Euler(-90, 0, 0)
-        );
+    void InstantiatePing (dynamic json) {
+        Vector2 position = new Vector2((float) json.position.x, (float) json.position.y);
+        // todo json.player
+        // todo json.type
+        Debug.Log("Player \"" + json.player + "\" pinged " + json.type + " at x=" + position.x + ", y=" + position.y);
+        UnityThread.executeInUpdate (() => {
+            Instantiate(
+                Ping,
+                new Vector3(position.x, 0, position.y),
+                Quaternion.Euler(-90, 0, 0)
+            );
+        });
     }
 }
