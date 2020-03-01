e9320251b23d17ef4e8a269f0fb6e6c79afe87d1
Arne <arne.zerndt@student.hpi.de>
2020-03-01 12:49:45 +0100

    Add Particale Light.

---
+++ Assets/FloorPlate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorPlate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
